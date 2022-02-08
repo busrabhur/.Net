f7d776f868ef7039e277a7aee0d152c44eb1289e
Büşra Buhur <61162439+busrabhur@users.noreply.github.com>
2022-02-08 19:42:06 +0300

    staticElements

---
+++ StaticStatement/staticStatement/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticStatement
+{
+    internal class Student
+    {
+        public static int studentNumber { get; set; }
+
+        public static int showStudentNumber()
+        {
+
+            return studentNumber;
+        }
+
+    }
+}
