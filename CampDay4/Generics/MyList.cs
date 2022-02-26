f53f5eea04736b91172738f4e8ac9ed83b28030f
Büşra Buhur <61162439+busrabhur@users.noreply.github.com>
2022-02-26 16:10:53 +0300

    generics

---
+++ CampDay4/Generics/MyList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class MyList<T> //generic class:ben bu classta T ile çalışacağım
+        //Bana bir tip vereceksin ben de aşağıdaki sınıf özelliklerinde ona göre yapılandırma yapacağım demek
+    {
+
+
+        public void Add(T item) //programcı bu classtan nesne tanımlarkem hangi tipi vermişsse onu kullansın 
+        {
+            //listeler aslında arka planda array yönetir.Bu classtan obje oluşturduğun an yani new lediğin anda
+            //bir array oluşabilmesi için Constructor içinde bir array oluşturman lazım
+
+        }
+    }
+}
